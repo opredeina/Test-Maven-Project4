0231ce78373397641c625928510ed151a0610bca
Olesya Predeina <OVpredeina@yandex.ru>
2024-07-28 23:14:09 +0500

    WebTest

--- src/test/java/Registration.java
+++ src/test/java/Registration.java
@@ -1,9 +1,11 @@
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
-public class Registration extends Data{
+public class Registration {
 
   private final By inputFirstName = By.xpath("//input[@id='firstName']");
   private final By inputLastName = By.xpath("//input[@id='lastName']");
@@ -12,13 +14,17 @@ public class Registration extends Data{
   private final By textareaСurrentAddress = By.xpath("//textarea[@id='currentAddress']");
   private final By btnJoin = By.xpath("//button[@type='submit']");
 
+  WebDriver driver = new ChromeDriver();
+  Data getDate = new Data();
+  Actions actions = new Actions(driver);
+
+  @AfterEach
+  void afterEach() {
+    driver.quit();
+  }
 
   @Test
   void registration(){
-
-    WebDriver driver = new ChromeDriver();
-    Data getDate = new Data();
-
     driver.get(getDate.getUrl);
     driver.manage().window().maximize();
     driver.findElement(inputFirstName).sendKeys(getDate.fistName);
@@ -26,6 +32,8 @@ public class Registration extends Data{
     driver.findElement(inputEmail).sendKeys(getDate.email);
     driver.findElement(inputMobile).sendKeys(getDate.mobile);
     driver.findElement(textareaСurrentAddress).sendKeys(getDate.currentAddress);
+    Actions actions = new Actions(driver);
+    actions.scrollByAmount(1500,1500);
     driver.findElement(btnJoin).click();
   }
 }
--- src/test/java/WebTest.java
+++ src/test/java/WebTest.java
@@ -2,14 +2,22 @@ import static org.openqa.selenium.By.xpath;
 
 import dev.failsafe.internal.util.Assert;
 import org.checkerframework.dataflow.qual.AssertMethod;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+
 public class WebTest {
   WebDriver driver = new ChromeDriver();
+
+  @AfterEach
+  void afterEach() {
+    driver.quit();
+  }
 
   @Test
   void webtestone() {
@@ -24,8 +32,12 @@ public class WebTest {
   void webtesttwo() {
     //Открываю
     driver.get("https://www.toolsqa.com/selenium-training/");
+    driver.manage().window().maximize();
     //Клик по кнопке
-    driver.findElement(By.xpath("//div[@class='col-12 col-lg-auto order-3 order-lg-1']//a[@class='navbar__tutorial-menu']")).click();
+    driver.findElement(xpath("//div[@class='col-12 col-lg-auto order-3 order-lg-1']//a[@class='navbar__tutorial-menu']")).click();
+
+    String getTitle = driver.getTitle();
+    Assertions.assertEquals(getTitle, "Tools QA - Selenium Training");
 
   }
 
@@ -33,11 +45,12 @@ public class WebTest {
   void webtestthree() {
     //Открываю
     driver.get("https://www.toolsqa.com/selenium-training/");
+    driver.manage().window().maximize();
     //Клик по About
     driver.findElement(By.xpath("//ul[@class='navbar__links d-none d-lg-flex']//a[@href='/about']")).click();
-    //На весь экран
-    driver.manage().window().maximize();
 
+    String getTitle = driver.getTitle();
+    Assertions.assertEquals(getTitle, "Tools QA | About Us");
   }
 
   @Test
@@ -46,7 +59,9 @@ public class WebTest {
     driver.manage().window().maximize();
     driver.navigate().to("https://demoqa.com/");
 
-    driver.quit();
+    String getTitle = driver.getTitle();
+    Assertions.assertEquals(getTitle, "DEMOQA");
+
   }
 
 }
